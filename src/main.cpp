d3c8b81296cfae435a96fce2a9ec69abc178ae6e
Piotr Rasztabiga <piotras826@student.polsl.pl>
2019-03-13 10:43:06 +0100

    Testing added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "MainConfig.h";
 using namespace std;
 
@@ -11,7 +12,7 @@ using namespace degreemath;
 #include <cmath>
 #endif
 
-int main () {
+void example() {
 	#ifdef USE_TRIGONOMETRY_DEGREE
 		std::cout << "Trygonometria w stopniach..." << std::endl;
 		std::cout << "Sinus 0 stopni: " << degreemath::sin(0) << std::endl;
@@ -35,5 +36,31 @@ int main () {
 	#endif
 
 	std::getchar();
+}
+
+int main (int argc, char* argv[]) {
+	if (argc == 1) {
+		example();
+		return 0;
+	}
+
+	if (argc < 3) 
+		std::cout << "Please provide math function type and argument" << std::endl;
+
+	double degrees = atof(argv[2]);
+	std::string arg(argv[1]);
+
+	if (arg == "--sin") {
+		std::cout << degreemath::sin(degrees);
+	} else if (arg == "--cos") {
+		std::cout << degreemath::cos(degrees);
+	} else if (arg == "--tg") {
+		std::cout << degreemath::tg(degrees);
+	} else if (arg == "--ctg") {
+		std::cout << degreemath::ctg(degrees);
+	} else {
+		std::cout << "Math function not recognized " << arg << std::endl;
+	}
+
 	return 0;
 }
